77570247159a36b6cf88abde210494f82e3cf53c
Kyle Pickard <123780748+SourceSailor@users.noreply.github.com>
2024-03-13 13:27:04 -0700

    Creation of Gericht Application
    Created application using Vite and installing React Icons.
    
    Created the basic structure of the application.
    
    Added site wide styles.
    
    Added data and assets
    
    Added initial styles and components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+import { useState } from "react";
+import React from "react";
 import "./App.css";
+
+import {
+  AboutUs,
+  Chef,
+  FindUs,
+  Footer,
+  Gallery,
+  Header,
+  Intro,
+  Laurels,
+  SpecialMenu,
+} from "./containers";
 
 function App() {
   return (
     <>
-      <h1>HELLO WORLD</h1>
+      <h1>Hello World</h1>
+      <AboutUs />
+      <Chef />
+      <FindUs />
+      <Footer />
+      <Gallery />
+      <Header />
+      <Intro />
+      <Laurels />
+      <SpecialMenu />
     </>
   );
 }
---
+++ src/components/Footer/FooterOverlay.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./footerOverlay.css";
+
+const FooterOverlay = () => {
+  return <div>FooterOverlay</div>;
+};
+
+export default FooterOverlay;
